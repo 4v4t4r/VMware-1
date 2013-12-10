eded329f18e0121939c1a38df10146c088db1851
Don Lewis <dlewis3713@gmail.com>
2013-12-10 14:56:18 -0800

    First Commit

---
+++ Get-VMToolsUpgradePolicy.ps1
@@ -0,0 +1,12 @@
+﻿
+
+$Report = "Tools-Upgrade-On-Boot-Report.csv"
+
+Write-Output "VMName,OS,ToolsUpgradePolicy" > $Report
+
+Get-Cluster | Where-Object {$_.Name -notlike "Merlin*"} | Get-VM | ForEach-Object {
+    $VM = $_.Name
+    $OS = $_.Guest.OsFullName
+    $ToolsPolicy = $_.ExtensionData.Config.Tools.ToolsUpgradePolicy
+    Write-Output "$VM,$OS,$ToolsPolicy" >> $Report
+    }
