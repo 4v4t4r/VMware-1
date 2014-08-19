3e45c710f536cf886ee4c99c39a181156c88e23d
Don Lewis <dlewis3713@gmail.com>
2014-08-19 09:02:00 -0700

    First commit

---
+++ Untitled13.ps1
@@ -0,0 +1,15 @@
+﻿$vms = Get-Content vms.txt
+$temp = "temp.csv"
+
+Write-Output "Name,Host,Cluster" > $temp
+
+ForEach ($vm in $vms) {
+    $data = Get-VM $vm
+    $Name = $data.Name
+    $VMHost = $data.VMHost.Name
+    $Cluster = $data.VMHost.Parent
+    Write-Output "$Name,$VMHost,$Cluster" >> $Temp
+}
+
+$Output = Import-Csv $temp
+$Output
