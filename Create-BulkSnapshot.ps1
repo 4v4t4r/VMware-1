8ac1462325fc2b6bb470acf61d2a7e1202c0ad84
Don Lewis <dlewis3713@gmail.com>
2014-12-11 16:57:24 -0800

    Script to create bulk snapshot and script to generate a report of existing snapshots.

---
+++ Create-BulkSnapshot.ps1
@@ -0,0 +1,12 @@
+﻿$VCenterServer = "ussypvcs04.ad.sjm.com"
+$File = "C:\Users\lewisd01\temp\MIR-Server-Patching-121114"
+$Servers = Get-Content $File
+$SnapshotName = "Before_Patching_121114"
+$SnapshotDescription = "Snapshot of servers before patches (DWL 12/11/2014)"
+$VCenterConnection = Connect-VIServer $VCenterServer
+
+ForEach($Server in $Servers) {
+    Get-VM $Server | New-Snapshot -Name $SnapshotName -Description $SnapshotDescription -Quiesce -RunAsync -Confirm:$false
+}
+
+Disconnect-VIServer $VCenterConnection.Id -Confirm:$false
---
+++ Get-VMSnapshotReport.ps1
@@ -0,0 +1,42 @@
+﻿$Report = "SnapshotReport.html"
+$CurrentDate = Get-Date
+$Creds = Get-Credential
+$VCConnection = Connect-VIServer uspychspvcs01.chs.com -Credential $Creds
+
+
+Write-Output "<table border=1 cellpadding=5 bordercolor=#006c56>
+    <tr>
+    <th colspan=2><FONT COLOR=#006c56><FONT FACE=Arial, sans-serif><FONT SIZE=2 STYLE=font-size: 14pt><B>On-Call Schedule</B></FONT></FONT></FONT></th>
+    </tr>
+    <tr>
+    <th><FONT COLOR=#006c56><FONT FACE=Arial, sans-serif><FONT SIZE=2 STYLE=font-size: 11pt><B>VM</B></FONT></FONT></FONT></th>
+    <th><FONT COLOR=#006c56><FONT FACE=Arial, sans-serif><FONT SIZE=2 STYLE=font-size: 11pt><B>Name</B></FONT></FONT></FONT></th>
+    <th><FONT COLOR=#006c56><FONT FACE=Arial, sans-serif><FONT SIZE=2 STYLE=font-size: 11pt><B>Description</B></FONT></FONT></FONT></th>
+    <th><FONT COLOR=#006c56><FONT FACE=Arial, sans-serif><FONT SIZE=2 STYLE=font-size: 11pt><B>Days Old</B></FONT></FONT></FONT></th>
+    <th><FONT COLOR=#006c56><FONT FACE=Arial, sans-serif><FONT SIZE=2 STYLE=font-size: 11pt><B>Created</B></FONT></FONT></FONT></th>
+    <th><FONT COLOR=#006c56><FONT FACE=Arial, sans-serif><FONT SIZE=2 STYLE=font-size: 11pt><B>SizeGB</B></FONT></FONT></FONT></th>
+    </tr>" > $Report
+
+Get-VM | Get-Snapshot | ForEach-Object {
+    $VM = $_.VM
+    $Name = $_.Name
+    $Description = $_.Description
+    $Created = $_.Created
+    $SizeGB = $_.SizeGB
+    $DaysOld = (New-TimeSpan -Start $Created -End $CurrentDate).Days
+    if ($DaysOld -gt 60){
+        Write-Output "<tr>
+            <td ALIGN=center><FONT FACE=Arial, sans-serif><FONT SIZE=2 STYLE=font-size: 11pt>$VM</FONT></FONT></P></td>
+            <td ALIGN=center><FONT FACE=Arial, sans-serif><FONT SIZE=2 STYLE=font-size: 11pt>$Name</FONT></FONT></P></td>
+            <td ALIGN=center><FONT FACE=Arial, sans-serif><FONT SIZE=2 STYLE=font-size: 11pt>$Description</FONT></FONT></P></td>
+            <td ALIGN=center><FONT FACE=Arial, sans-serif><FONT SIZE=2 STYLE=font-size: 11pt>$DaysOld</FONT></FONT></P></td>
+            <td ALIGN=center><FONT FACE=Arial, sans-serif><FONT SIZE=2 STYLE=font-size: 11pt>$Created</FONT></FONT></P></td>
+            <td ALIGN=center><FONT FACE=Arial, sans-serif><FONT SIZE=2 STYLE=font-size: 11pt>$SizeGB</FONT></FONT></P></td>
+            </tr>" >> $Report
+    }
+    Else {Write-Output "Do nothing..." > $null}
+}
+
+Send-MailMessage -Subject $Subject -To dlewis@sjm.com -From chs-noreply@chs.com -Body (Get-Content $Report | Out-String) -BodyAsHtml
+
+Disconnect-VIServer $VCConnection.Id -Confirm:$false
